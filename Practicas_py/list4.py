6ff73b57ff59299bfc3ab35704b9bd29be19e143
Esau-Rodriguez <250387@utxicotepec.edu.mx>
2026-09-17 13:29:36 -0600

    Recorrido de lista y seccionamiento de lista y cadenas de texto

--- Practicas_py/list4.py
+++ Practicas_py/list4.py
@@ -1,4 +1,16 @@
-#Recorrido de lista
+#Recorrido de lista de izquierda derecha pero asigna una variable temporal
 lista = [10,20,30,40,50,60]
-for x in lista:
-    print(x)
+#for x in lista:
+    #print(x)
+
+#Recorrido con el indice
+#for i in range(len(lista)):
+    #print(lista[i])
+
+print(lista[1])#Imprime el valor que se enuentra en el indice 1
+print(lista[::-1])#Recorrido de derecha a izquierda
+print(lista[1:5])#Ecluye el indice 0 y termina una posicion antes del indice 5
+
+#Seccionamiento de cadena
+cadena = "¡Hola, Python!"
+print(cadena[0:8])
