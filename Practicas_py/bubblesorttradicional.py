b4f44ad1971b5db2e648916bfb78d6f3844e8c1b
Esau-Rodriguez <250387@utxicotepec.edu.mx>
2026-09-17 14:12:29 -0600

    Ordenamiento con bubble sort con el metodo tradicional con un for anidado

---
+++ Practicas_py/bubblesorttradicional.py
@@ -0,0 +1,12 @@
+#Ordenamiento con bubble sort metodo tradicional
+lista=[12,10,8,0,52,0]
+print(lista)
+for i in range(len(lista)):
+    for x in range(len(lista)-1):
+        if lista[x] > lista[x+1]:
+            aux = lista[x]
+            lista[x] = lista[x+1]
+            lista[x+1] = aux
+            print(lista)
+
+
