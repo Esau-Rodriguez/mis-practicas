1f51874a740e25f98feeefd0dec04504ffe98eb1
Esau-Rodriguez <250387@utxicotepec.edu.mx>
2026-09-22 10:46:49 -0600

    Practica de ordenamiento ascendente y descendente con bubble sort

---
+++ Practicas_py/practica2_burbuja.py
@@ -0,0 +1,27 @@
+calificaciones = [8,7,9,10,6,7,9,8,7,10,9,10,8,10,7]
+n = len(calificaciones)
+swapped = True
+
+while  swapped:
+    swapped = False
+    for i in range(n-1):
+        if calificaciones[i]>calificaciones[i+1]:
+            calificaciones[i],calificaciones[i+1]=calificaciones[i+1],calificaciones[i]
+            swapped = True
+print("Orden ascendente: ",calificaciones)
+
+while  swapped:
+    swapped = False
+    for i in range(n-1):
+        if calificaciones[i]<calificaciones[i+1]:
+            calificaciones[i],calificaciones[i+1]=calificaciones[i+1],calificaciones[i]
+            swapped = True
+print("Orden descendente: ",calificaciones)
+
+for i in range(len(calificaciones)):
+    for x in range(len(calificaciones)-1):
+        if calificaciones[x]<calificaciones[x+1]:
+            aux = calificaciones[x]
+            calificaciones[x]=calificaciones[x+1]
+            calificaciones[x+1]=aux
+print(calificaciones)
