d814103935c01386fd5d9fdbabd3f2cae3f5c8c4
Esau-Rodriguez <250387@utxicotepec.edu.mx>
2026-09-22 10:51:45 -0600

    Practica de ordenamiento ascendente y descendente con bubble sort

--- Practicas_py/practica2_burbuja.py
+++ Practicas_py/practica2_burbuja.py
@@ -2,6 +2,7 @@ calificaciones = [8,7,9,10,6,7,9,8,7,10,9,10,8,10,7]
 n = len(calificaciones)
 swapped = True
 
+#Ordenamiento ascendente
 while  swapped:
     swapped = False
     for i in range(n-1):
@@ -10,6 +11,7 @@ while  swapped:
             swapped = True
 print("Orden ascendente: ",calificaciones)
 
+#Ordenamiento descendente
 while  swapped:
     swapped = False
     for i in range(n-1):
@@ -18,6 +20,7 @@ while  swapped:
             swapped = True
 print("Orden descendente: ",calificaciones)
 
+#Descendente con metodo tradicional
 for i in range(len(calificaciones)):
     for x in range(len(calificaciones)-1):
         if calificaciones[x]<calificaciones[x+1]:
